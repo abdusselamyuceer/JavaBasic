8ad30d2266647b29cba91e23bdc49cc9199f613c
selam123 <asyuceer@gmail.com>
2019-07-03 13:42:08 +0300

    Add files via upload

---
+++ PrintlnJv.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubjavabasic;
+
+/**
+ *
+ * @author abdusselam.yuceer
+ */
+public class PrintlnJv {
+        public void Printstr(){
+            System.out.println("Welcome to Java");
+            System.out.println("Learning Java now");
+            System.out.println("Programming is fun");
+        }    
+}
