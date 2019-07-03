658b02a16e03ee2001c07072eca9858cf8b8eb32
selam123 <asyuceer@gmail.com>
2019-07-03 15:07:24 +0300

    Add files via upload

---
+++ PrintJava.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubjavabasic;
+
+/**
+ *
+ * @author abdusselam.yuceer
+ */
+public class PrintJava {
+    public void printjava(){
+        System.out.println("  J");
+        System.out.println("J  aaa   v   vaaa");
+        System.out.println("J  J aa   v v  a a");
+        System.out.println("J   aaaa   v   aaaa");
+    }
+    
+}
---
+++ PrintSameMessages.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubjavabasic;
+/**
+ *
+ * @author abdusselam.yuceer
+ */
+public class PrintSameMessages {
+   public void printjava(){
+       int i;
+       for(i = 0; i < 5; i++){
+           System.out.println("I love Java");
+       }
+   }    
+}
